7dc812d7029384dac093b7fa5b377cdb51f40473
Asish kumar Panda <112423732+asishpanda2003@users.noreply.github.com>
2025-08-25 15:13:05 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = https://github-backend-4iie.onrender.com/api;
 
 export const searchRepositories = (term, pageNum) => {
     return axios.get(`${API_BASE_URL}/search`, {
